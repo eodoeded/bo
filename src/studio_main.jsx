5e7433c9dad17664a8cdee87f3dc1efeeaca4ad8
eodoeded <eodoeded@users.noreply.github.com>
2025-11-27 21:45:55 +0000

    Fix studio header duplication and Ethicronics arrow character

--- src/studio_main.jsx
+++ src/studio_main.jsx
@@ -1,15 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './studio/App.jsx'
-import Header from './components/Header.jsx'
 
 if ('scrollRestoration' in history) { history.scrollRestoration = 'manual'; }
 window.scrollTo(0, 0);
 
 function StudioApp() {
   return (
-    <div className="bg-[#12110D] min-h-screen w-full overflow-x-hidden">
-      <Header />
+    <div className="bg-[#FAFAFA] min-h-screen w-full overflow-x-hidden">
       <App />
     </div>
   )
