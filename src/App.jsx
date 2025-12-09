58f00902768e4079cefc8fd13eb67ee8a35c851f
eodoeded <eodoeded@users.noreply.github.com>
2025-12-09 11:31:30 +0000

    Remove Footer component, keeping only Divider as copy-email footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Hero from './components/Hero'
 import Proceses from './components/Proceses'
 import GridSection from './components/GridSection'
 import Divider from './components/Devider'
-import Footer from './components/Footer'
 
 function App() {
   return (
@@ -15,7 +14,6 @@ function App() {
          <Proceses />
          <GridSection />
          <Divider />
-         <Footer />
     </>
   )
 }
